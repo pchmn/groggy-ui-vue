a8681d79bb8e26d93a178e21b1f906e4b8639b77
pchmn <pchmn.dev@gmail.com>
2021-06-27 01:41:02 +0200

    feat(checkbox): add size, round and inderteminate props [skip ci]

--- packages/@groggy-ui-vue/src/components/checkbox/Checkbox.vue
+++ packages/@groggy-ui-vue/src/components/checkbox/Checkbox.vue
@@ -1,25 +1,22 @@
 <template>
   <label class="inline-flex items-center">
     <input
+      v-tw="'form-checkbox'"
+      :class="classes.checkboxClasses"
       type="checkbox"
-      :class="rootClasses"
       :disabled="disabled"
       :checked="modelValue"
+      :indeterminate="indeterminate"
       @change="$emit('update:modelValue', !modelValue)"
     />
-    <span :class="labelClassses"><slot></slot></span>
-    <!-- <span
-      class="ml-2 font-normal"
-      @click="$emit('update:modelValue', !modelValue)"
-      ><slot></slot
-    ></span> -->
+    <span :class="classes.labelClasses"><slot></slot></span>
   </label>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, PropType, computed } from 'vue';
+import { defineComponent, PropType, computed } from 'vue';
 import { Variant, Size } from '@models/common-props.types';
-import { useTheme } from '@themes/useTheme';
+import { useComponentClasses } from '@themes/hooks/useComponentClasses';
 
 export default defineComponent({
   name: 'Checkbox',
@@ -29,36 +26,20 @@ export default defineComponent({
       type: String as PropType<Variant>,
       default: 'default',
     },
+    size: {
+      type: String as PropType<Size>,
+      default: 'md',
+    },
+    round: Boolean,
     disabled: Boolean,
+    indeterminate: Boolean,
   },
   emits: ['update:modelValue'],
-  setup: (props, { emit }) => {
-    const { theme } = useTheme();
-    const rootClasses = computed(() => [
-      theme.components.checkbox.base,
-      props.disabled
-        ? `${theme.components.checkbox.disabled} disabled`
-        : theme.components.checkbox.variant(props.variant),
-    ]);
-    const labelClassses = computed(() => [
-      theme.components.checkbox.label,
-      {
-        [`${theme.components.checkbox.labelDisabled}`]: props.disabled,
-      },
-    ]);
-    return { rootClasses, labelClassses };
+  setup: (props) => {
+    const classes = computed(() => useComponentClasses('checkbox', props));
+    return { classes };
   },
 });
 </script>
 
-<style scoped>
-input {
-  background-size: 0%;
-}
-input[type='checkbox']:checked {
-  background-size: 100%;
-}
-input[type='checkbox']:checked.disabled {
-  background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='rgba(255, 255, 255, 0.5)' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12.207 4.793a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-2-2a1 1 0 011.414-1.414L6.5 9.086l4.293-4.293a1 1 0 011.414 0z'/%3e%3c/svg%3e");
-}
-</style>
+<style scoped></style>
